f3d3d5752d25e8d2826e1b3968bceb0f91f5986d
sid_venkat <venkat@genie-enterprise.com>
2025-11-17 13:49:15 +0100

    changes in version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ Simple Flask API that uses psycopg2 (requires system libraries)
 """
 from flask import Flask, jsonify
 import psycopg2
-import requester
+import requests
 #Delete a comment
 
 app = Flask(__name__)
